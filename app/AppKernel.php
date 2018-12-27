e4460ff18bf242c37360f559b1dd48971f42ca4e
demot <wojtek13radom@gmail.com>
2018-12-27 16:55:31 +0100

    typo

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,7 +26,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Matthimatiker\OpcacheBundle\MatthimatikerOpcacheBundle();;
+            $bundles[] = new Matthimatiker\OpcacheBundle\MatthimatikerOpcacheBundle();
 
             if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
