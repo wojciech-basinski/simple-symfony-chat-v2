dba94af689a20325a2f313b5f6c6b03d6c9443de
demot <wojtek13radom@gmail.com>
2019-01-03 16:26:16 +0100

    added new line to quote message

---
+++ src/AppBundle/Utils/Banned.php
@@ -0,0 +1,53 @@
+<?php
+namespace AppBundle\Utils;
+
+use AppBundle\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+class Banned
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    /**
+     * @var User|null
+     */
+    private $user = null;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function getReason(string $userName): ?string
+    {
+        $user = $this->getUser($userName);
+        return $user->getBanReason();
+    }
+
+    public function removeBan(string $userName): void
+    {
+        $user = $this->getUser($userName);
+        $user->setBanned(null)
+            ->setBanReason(null);
+        $this->em->persist($user);
+        $this->em->flush();
+    }
+
+    public function getTime(string $userName): ?\DateTime
+    {
+        $user = $this->getUser($userName);
+        return $user->getBanned();
+    }
+
+    private function getUser(string $userName): User
+    {
+        if ($this->user !== null) {
+            return $this->user;
+        }
+        $this->user = $this->em->getRepository(User::class)->findOneByUsername($userName);
+        return $this->user;
+    }
+}
